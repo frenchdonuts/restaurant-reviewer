f06a73f831b8e9ab38b9360efd59198dca6d6475
frenchdonuts <jonathantan1991@gmail.com>
2017-01-12 08:31:53 -0500

    feat: Add service worker code

---
+++ src/js/sw.js
@@ -0,0 +1,44 @@
+var staticCacheName = 'restaurant-reviewer-static-v1'
+
+const {
+    assets,
+} = serviceWorkerOption
+
+let assetsToCache = [
+    ...assets,
+    './',
+].map((path) => {
+    return new URL(path, location).toString();
+})
+
+
+self.addEventListener('install', event =>
+    event.waitUntil(
+        caches.open(staticCacheName).then(cache =>
+            cache.addAll(assetsToCache)
+        )
+    )
+)
+
+self.addEventListener('activate', event =>
+    event.waitUntil(
+        caches.keys().then(cacheNames =>
+            Promise.all(
+                cacheNames.filter(cacheName =>
+                    cacheName.startsWith('restaurant-reviewer-') &&
+                    cacheName != staticCacheName
+                ).map(cacheName =>
+                    caches.delete(cacheName)
+                )
+            );
+        )
+    )
+);
+
+self.addEventListener('fetch', event => {
+    //
+    event.respondWith(
+        caches.match(event.request).then(cachedResponse =>
+            cachedResponse || fetch(event.request))
+    )
+})
