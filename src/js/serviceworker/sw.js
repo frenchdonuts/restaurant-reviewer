eaf8b7f7e784c62a1fef26e20d712f1636a237b0
frenchdonuts <jonathantan1991@gmail.com>
2017-01-12 11:19:17 -0500

    feat: Cache fonts and css on servie worker install event; Start on caching imgs as well

--- src/js/serviceworker/sw.js
+++ src/js/serviceworker/sw.js
@@ -1,13 +1,26 @@
 var staticCacheName = 'restaurant-reviewer-static-v1'
+var contentImgsCache = 'restaurant-reviewer-imgs'
+var allCaches = [
+    staticCacheName,
+    contentImgsCache
+]
 
 const {
     assets,
 } = serviceWorkerOption
 
+console.log(assets)
 let assetsToCache = [
     ...assets,
-    './',
+    '/',
+    'https://fonts.googleapis.com/css?family=Roboto:400,300,500|Roboto+Mono|Roboto+Condensed:400,700&subset=latin,latin-ext',
+    'https://fonts.gstatic.com/s/roboto/v15/RxZJdnzeo3R5zSexge8UUZBw1xU1rKptJj_0jans920.woff2',
+    'https://fonts.gstatic.com/s/materialicons/v19/2fcrYFNaTjcS6g4U3t-Y5UEw0lE80llgEseQY3FEmqw.woff2',
+    'https://fonts.gstatic.com/s/roboto/v15/oMMgfZMQthOryQo9n22dcuvvDin1pK8aKteLpeZ5c0A.woff2',
+    'https://fonts.googleapis.com/icon?family=Material+Icons',
+    'https://code.getmdl.io/1.2.1/material.indigo-orange.min.css'
 ].map((path) => {
+    console.log(path)
     return new URL(path, location).toString();
 })
 
@@ -26,7 +39,7 @@ self.addEventListener('activate', event =>
             Promise.all(
                 cacheNames.filter(cacheName =>
                     cacheName.startsWith('restaurant-reviewer-') &&
-                    cacheName != staticCacheName
+                    !allCaches.includes(cacheName)
                 ).map(cacheName =>
                     caches.delete(cacheName)
                 )
@@ -35,10 +48,14 @@ self.addEventListener('activate', event =>
     )
 );
 
+console.log("Adding fetch event listener")
 self.addEventListener('fetch', event => {
     //
+    var requestUrl = new URL(event.request.url)
     event.respondWith(
-        caches.match(event.request).then(cachedResponse =>
-            cachedResponse || fetch(event.request))
+        caches.match(event.request).then(cachedResponse => {
+            console.log("hey!")
+            return cachedResponse || fetch(event.request)
+        })
     )
 })
