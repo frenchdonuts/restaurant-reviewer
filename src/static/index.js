1675bd2c6e5e237614ba2acca65cd58840a1c288
frenchdonuts <jonathantan1991@gmail.com>
2017-01-12 08:44:03 -0500

    feat: Register service worker

--- src/static/index.js
+++ src/static/index.js
@@ -1,3 +1,5 @@
+import runtime from 'serviceworker-webpack-plugin/lib/runtime' // Service Worker
+
 // pull in desired CSS/SASS files
 require('./styles/main.css');
 
@@ -6,3 +8,12 @@ var Elm = require('../elm/Main');
 var app = Elm.Main.fullscreen();
 
 app.ports.setTimezoneOffset.send(new Date().getTimezoneOffset())
+
+// Setup serviceWorker
+if ('serviceWorker' in navigator) {
+    var swConfig = {
+        scope: '/'
+    }
+
+    runtime.register()
+}
