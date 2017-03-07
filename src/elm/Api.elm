2753beacd60ea498ae8a3e5de9b991a53317d748
frenchdonuts <jonathantan1991@gmail.com>
2017-03-07 18:01:58 -0500

    chore: Change proxy_url to point to ubuntu_512mb_nyc3_01. Re-build dist

--- src/elm/Api.elm
+++ src/elm/Api.elm
@@ -14,7 +14,7 @@ import Time.DateTime as Time
 
 
 proxy_url : String
-proxy_url = "http://jt-restaurant-reviewer.herokuapps.com"
+proxy_url = "http://45.55.228.192:8080"
 
 getRestaurants : Float -> Float -> Cuisine -> Model -> Http.Request (List RestaurantPreview)
 getRestaurants lat long selectedCuisine model =
